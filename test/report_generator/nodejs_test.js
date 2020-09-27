776d238efd33ea94bc6f0eddbe5dccde22950604
cos <cosmineugendinu@gmail.com>
2020-09-28 01:07:41 +0300

    refactor: Log function with closure state and test

--- test/report_generator/nodejs_test.js
+++ test/report_generator/nodejs_test.js
@@ -4,14 +4,11 @@ const assert = require('assert');
 const rewire = require('rewire');
 const Code = rewire('../../report_generator/Code.js');
 
+
 const libraryGet = Code.__get__('libraryGet');
 
 const tests = new Map();
 
-tests.set('dummy_func',() => {
-  const dummy_func = libraryGet('dummy_func');
-  assert.equal(dummy_func(10), 20, 'dummy_func failed');
-});
 
 tests.set('FieldValidator', () => {
   const FieldValidator = libraryGet('FieldValidator');
@@ -55,7 +52,51 @@ tests.set('FieldValidator', () => {
     {name: 'Error', reason: 'readonly'});
 });
 
+tests.set('Log', () => {
+  // mocks
+  const cellValue = []
+  const cell = {setValue(msg){cellValue[0] = msg;}, setBackground(c){}, setFontColor(c){}, setVerticalAlignment(a){}};
+  const range = {clear(){return this;}, merge(){return this;}, getCell(x,y){return cell;},};
+  const sheet = {getRange(x,y,z,w){return range;}};
+  
+  const Log = libraryGet('Log');
 
+  let cellPos = [1,1], defaultVerbosity = 3;
+  let [v, log] = Log(sheet, cellPos, defaultVerbosity);
+  let messages = ['ceva', 'altceva'];
+  v(1); log(messages[0]); log(messages[1]);
+  assert.equal(cellValue[0], '> '+messages.join('\n> '));
 
-tests.get('dummy_func')();
-tests.get('FieldValidator')();
+  cellValue[0] = undefined;
+  [v, log] = Log(sheet, cellPos, 2);
+  messages = ['cva'];
+  v(2); log(messages[0]);
+  assert.equal(cellValue[0], '> '+messages.join('\n> '));
+
+  cellValue[0] = undefined;
+  [v, log] = Log(sheet, cellPos, 1);
+  messages = ['cva'];
+  v(1); log(messages[0]);
+  assert.equal(cellValue[0], '> '+messages.join('\n> '));
+
+  cellValue[0] = undefined;
+  [v, log] = Log(sheet, cellPos, 1);
+  messages = ['cva'];
+  v(2); log(messages[0]);
+  // because 2 from 'v(2)' is >= 1 from defaultVerbosity
+  assert.equal(cellValue[0], undefined);
+  v(1); log('important');
+  // this is logged because it is important! kidding...
+  assert.equal(cellValue[0], '> important');
+});
+
+tests.set('dummy_func',() => {
+  const libScopeVar = 'lsv';
+  const dummy_func = libraryGet('dummy_func');
+  assert.equal(dummy_func('arg'), 'arg_lsv', 'dummy_func failed');
+});
+
+//tests.get('dummy_func')();
+//tests.get('FieldValidator')();
+tests.get('Log')();
+
