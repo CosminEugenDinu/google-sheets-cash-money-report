949fd9cf26ef580c3334cb89a0accd49c34c0f6e
cos <cosmineugendinu@gmail.com>
2020-10-02 13:37:22 +0300

    refactor: Test null and undefined arguments for FieldValidator.setField

--- test/report_generator/nodejs_test.js
+++ test/report_generator/nodejs_test.js
@@ -139,8 +139,14 @@ tests.set('FieldValidator', () => {
   const testArguments = {
     setField:{
       correctArgs: [
-        ['nums',0,'number'],
-        ['nums',0,'number',0],
+        ['nums0',0,'number'],
+        ['nums1',0,'number',undefined],
+        ['nums',0,'number',undefined,undefined],
+        ['nums',0,'number',undefined,undefined,undefined],
+        ['nums2',0,'number',undefined,2],
+        ['nums',0,'number',2,undefined],
+        ['nums',0,'number',undefined,null],
+        ['nums',0,'number',null,undefined],
         ['nums',0,'number',null],
         ['nums',0,'number',null,2],
         ['nums',0,'number',0,null],
@@ -149,6 +155,7 @@ tests.set('FieldValidator', () => {
         ['nums',0,'number',null,null],
         ['nums',0,'number',null,null,null],
         ['nums',0,'number',null,null,exactIntValues],
+        ['nums',0,'number',undefined,undefined,exactIntValues],
       ],
       wrongNumOfArgs: [
         [],
@@ -188,8 +195,12 @@ tests.set('FieldValidator', () => {
   // test correct arguments
   for (const args of testArguments.setField.correctArgs){
     assert.doesNotThrow(()=>{
-      new FieldValidator().setField(...args); 
-    },JSON.stringify(args))
+      try { new FieldValidator().setField(...args); }
+      catch (e) {
+        console.log(JSON.stringify(e));
+        throw e;
+      }
+    }, `should not throw if args are ${args}`)
   }
   // test wrong number of arguments
   for (const args of testArguments.setField.wrongNumOfArgs){
